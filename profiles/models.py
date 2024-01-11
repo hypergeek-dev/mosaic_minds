4d062d1c5440fc7cc10c233f6ac2691a508c03e3
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-11 21:25:40 +0000

    Adding phonenumber and email fields to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -6,12 +6,15 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=255, blank=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=255, blank=True)
 
 
     class Meta:
         ordering = ['-created_at']
+        db_table = 'mosaicminds.profiles'
 
     def __str__(self):
         return f"{self.owner}'s profile"
