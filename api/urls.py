0d5629ab2b1315dd4f343fae7974b7cdeda0b2ba
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-11 22:04:10 +0000

    re-adding paths since merge conflict

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include, include
+from django.urls import path, include
+from meetings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('profiles/', include('profiles.urls')),  
+    path('meetings/', include('meetings.urls')),
+     path('meetings/', views.MeetingList.as_view(), name='meeting_list'),
+    path('meetings/<int:pk>/', views.MeetingDetail.as_view(), name='meeting_detail'),
+    
 ]
--- meetings/urls.py
+++ meetings/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from meetings import views
 from django.urls import path
-from .views import MeetingList 
+
 
 urlpatterns = [
-    path('', MeetingList.as_view(), name='meeting-list'),
     path('meetings/', views.MeetingList.as_view()),
     path('meetings/<int:pk>/', views.MeetingDetail.as_view())
 ]
