17150183733d6d1b407cf1c8f504dfec103cd08d
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-11 21:25:18 +0000

    Adding endpoint to meetinglist in urls.py

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from meetings import views
+from django.urls import path
+from .views import MeetingList 
 
 urlpatterns = [
+    path('', MeetingList.as_view(), name='meeting-list'),
     path('meetings/', views.MeetingList.as_view()),
     path('meetings/<int:pk>/', views.MeetingDetail.as_view())
 ]
