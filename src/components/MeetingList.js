6717aec383151a15fb5478bd5adcaf7fa2ff8bd2
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-12 23:38:02 +0000

    MeetingList component

---
+++ src/components/MeetingList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MeetingList() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Placehoder for list of meeting</p>
+           
+        </div>
+    );
+}
+
+export default MeetingList;
