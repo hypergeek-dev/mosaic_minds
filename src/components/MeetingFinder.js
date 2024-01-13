9736b86c6ce023613bf69fa985c1a49884b120c2
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-13 08:35:32 +0000

    Adding Meetingfindercomponent with placeholder

---
+++ src/components/MeetingFinder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MeetingFinder() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Placeholder for the meeting finder</p>
+        </div>
+    );
+}
+
+export default MeetingFinder;
