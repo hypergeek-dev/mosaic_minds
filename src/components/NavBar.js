d695de73ae6eece0498eea8dc9d310c13ba73b28
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-13 20:40:12 +0000

    Adding working paths to navbar, connecting components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,41 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
-      <Container>
-        <Navbar.Brand>
+    <Navbar expand="md">
+    <Container>
+      <Navbar.Brand as={Link} to="/">
 
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
-            <Nav.Link>
-              Home
-            </Nav.Link>
-            <Nav.Link>
-              About Us
-            </Nav.Link>
-            <Nav.Link>
-              Meetings
-            </Nav.Link>
-            <Nav.Link>
-              Volunteer
-            </Nav.Link>
-            <Nav.Link>
-              Sign in
-            </Nav.Link>
-            <Nav.Link>
-             Sign up
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto text-left">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/AboutUs">
+            About Us
+          </Nav.Link>
+          <Nav.Link as={Link} to="/MeetingList">
+            Meetings
+          </Nav.Link>
+          <Nav.Link as={Link} to="/Volunteer">
+            Volunteer
+          </Nav.Link>
+          <Nav.Link as={Link} to="/signin">
+            Sign in
+          </Nav.Link>
+          <Nav.Link as={Link} to="/signup">
+            Sign up
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
 };
+
 
 export default NavBar;
