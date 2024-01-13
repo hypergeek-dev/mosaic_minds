b5af098f99b31b1e430e6155c7f6bc025acf4edb
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-13 23:52:50 +0000

    Changing how area is fetched in serializers

--- meetings/serializer.py
+++ meetings/serializer.py
@@ -1,19 +1,17 @@
-from django.db import models
 from rest_framework import serializers
 from .models import Meeting
 
-
-
 class MeetingSerializer(serializers.ModelSerializer):
     added_by_username = serializers.SerializerMethodField()
-    is_owner = serializers.SerializerMethodField()  
-
+    is_owner = serializers.SerializerMethodField()
+    area_display = serializers.SerializerMethodField()  
+    weekday_display = serializers.SerializerMethodField() 
     class Meta:
         model = Meeting
         fields = [
             'meeting_id', 'name', 'meeting_time', 'approved', 'area',
             'description', 'online_meeting_url', 'added_by', 'added_by_username',
-            'is_owner' 
+            'is_owner', 'area_display', 'weekday_display'  
         ]
         extra_kwargs = {'added_by': {'read_only': True}}
 
@@ -26,6 +24,10 @@ class MeetingSerializer(serializers.ModelSerializer):
             return obj.added_by == request.user
         return False
 
+    def get_area_display(self, obj):
+        return obj.get_area_display()  
+    def get_weekday_display(self, obj):
+        return obj.get_weekday_display() 
     def create(self, validated_data):
         validated_data['added_by'] = self.context['request'].user
         return Meeting.objects.create(**validated_data)
