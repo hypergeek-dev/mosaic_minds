d3fc86ccfd18e8f047960b66fa7c8e242de5e2c9
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-14 16:17:16 +0000

    Adding delete definition on model

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,18 +12,19 @@ class Profile(models.Model):
     role_at_meeting = models.CharField(max_length=255, blank=True)
     comment = models.CharField(max_length=255, blank=True)
 
-
     class Meta:
         ordering = ['-created_at']
         db_table = 'mosaicminds.profiles'
 
     def __str__(self):
-        return f"{self.owner}'s profile"
+        return f"{self.owner.username}'s profile"
 
+    def delete(self, *args, **kwargs):
+        self.owner.delete()  
+        super().delete(*args, **kwargs)  
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
-
 
 post_save.connect(create_profile, sender=User)
