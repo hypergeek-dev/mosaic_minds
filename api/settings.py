a8613fb105c730af77f8e9397cabe7f242153cd0
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-14 10:37:33 +0000

    removing duplicate code from settings

--- api/settings.py
+++ api/settings.py
@@ -104,32 +104,15 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Allow all origins in development
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True  
 
 CORS_ALLOWED_ORIGINS = [
    "https://mosaicminds-1df2672b33ce.herokuapp.com",
-   'https://8000-hypergeekde-mosaicminds-ir8nzuiphif.ws-eu107.gitpod.io/',
-   ' http://127.0.0.1:8000/',
+   "https://8000-hypergeekde-mosaicminds-3pb1o54fche.ws-eu107.gitpod.io",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://mosaicminds-1df2672b33ce.herokuapp.com",
-   'https://8000-hypergeekde-mosaicminds-ir8nzuiphif.ws-eu107.gitpod.io/',
-   ' http://127.0.0.1:8000/',
+    "https://8000-hypergeekde-mosaicminds-ir8nzuiphif.ws-eu107.gitpod.io",
+    "http://127.0.0.1:8000",
 ]
-
-
-# Allow all origins in development
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-   "https://mosaicminds-1df2672b33ce.herokuapp.com",
-   'https://8000-hypergeekde-mosaicminds-3pb1o54fche.ws-eu107.gitpod.io',
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://mosaicminds-1df2672b33ce.herokuapp.com",
-   'https://8000-hypergeekde-mosaicminds-3pb1o54fche.ws-eu107.gitpod.io',
-]
-
