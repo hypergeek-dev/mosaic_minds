1d875283f615853bcef4f5eec6aee65e2a5045e0
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-14 10:38:55 +0000

    Removing redundant datafields from serializer

--- profiles/serializer.py
+++ profiles/serializer.py
@@ -12,7 +12,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'id', 'owner', 'created_at', 'name',
              'is_owner'
         ]
 
