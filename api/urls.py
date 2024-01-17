cb372ae9b02ac33c7f1e5c9c5387ce82d6c62466
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-17 13:51:32 +0000

    Changing url paths to fit new apps

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,33 @@
+"""drf_api URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import path, include
-from meetings import views as meetings_views
-from users import views as users_views
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from .views import logout_route
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('meetings/', include('meetings.urls')),
-    path('api-token-auth/', obtain_jwt_token),
-    path('api-token-refresh/', refresh_jwt_token),
-    path('api-token-verify/', verify_jwt_token),
+    path('api/api-auth/', include('rest_framework.urls')),
+    path('api/dj-rest-auth/logout/', logout_route),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/', include('profiles.urls')),
+    path('api/', include('meetings.urls')),
+    path('api/', include('favorites.urls')),
 ]
+
+handler404 = TemplateView.as_view(template_name='index.html')
