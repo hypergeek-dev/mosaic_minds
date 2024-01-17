ad626ec7358a271b00a5e3d7cdbca970305cc666
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-17 13:48:32 +0000

    Renaming import in meetings to follow naming convention changes

--- meetings/views.py
+++ meetings/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.http import Http404
 from .models import Meeting
-from .serializer import MeetingSerializer
+from .serializers import MeetingSerializer
 from api.permissions import IsOwnerOrReadOnly
 
 class MeetingList(ListAPIView):
