87ec084ca37b3fbb0aa197664f9cd10d656e3f6e
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-17 13:46:19 +0000

    Removing users

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_user, register_user, current_user
-
-urlpatterns = [
-    path('register/', register_user, name='register_user'),
-    path('login/', login_user, name='user_login'), 
-    path('current/', current_user, name='current_user'),
-]
