212e679503071970c88f910e6922b9ed9eb105bd
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-17 07:19:01 +0000

    Adding CSRF and JWT authentication to main app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect, createContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import axios from 'axios';
 import "./Global.css";
 import NavBar from "./components/NavBar";
 import Home from "./components/LandingPageHero";
@@ -7,42 +8,65 @@ import AboutUs from "./components/AboutUs";
 import MeetingList from "./components/MeetingList";
 import Volunteer from "./components/Volunteer";
 import MeetingSearchPage from "./components/MeetingSearchPage";
-import SignupForm from "./components/SignUpForm";
+import SignupForm from "./auth/SignUpForm";
+import EventForm from './components/AddMeeting';
 
-function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i].trim();
-          if (cookie.substring(0, name.length + 1) === (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-          }
-      }
-  }
-  return cookieValue;
-}
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('mosaicminds');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+});
 
-// Set the CSRF token for Axios POST requests
-axios.defaults.headers.post['X-CSRFToken'] = getCookie('csrftoken');
+export const CurrentUserContext = createContext();
 
 function App() {
-return (
-  <Router>
-    <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/MeetingList" element={<MeetingList />} />
-        <Route path="/MeetingSearchPage" element={<MeetingSearchPage />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/Volunteer" element={<Volunteer />} />
-        <Route path="/SignUpForm" element={<SignupForm />} />
-      </Routes>
-    </div>
-  </Router>
-);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [error, setError] = useState(null); 
+
+  const navigate = useNavigate(); 
+
+  const handleMount = async () => {
+    try {
+      const token = localStorage.getItem('mosaicminds'); 
+      if (token) {
+        const { data } = await axios.get('/users/current/', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        setCurrentUser(data);
+      } else {
+        navigate('/users/login'); 
+        }
+    } catch (err) {
+      console.error(err);
+      setError(err.message); 
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  return (
+    <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
+      <Router>
+        <div className="App">
+          <NavBar />
+          {error && <div className="error-message">{error}</div>}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/MeetingList" element={<MeetingList />} />
+            <Route path="/MeetingSearchPage" element={<MeetingSearchPage />} />
+            <Route path="/AboutUs" element={<AboutUs />} />
+            <Route path="/Volunteer" element={<Volunteer />} />
+            <Route path="/SignUpForm" element={<SignupForm />} />
+            <Route path="/AddMeeting" element={<EventForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </CurrentUserContext.Provider>
+  );
 }
 
 export default App;
