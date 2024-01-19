a8b5be7aacc6850f077c7ae3fd36b338e3425b2d
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-19 13:29:13 +0000

    Changing paths to fix faulty api request

--- api/urls.py
+++ api/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/', include('profiles.urls')),
     path('api/', include('meetings.urls')),
-    path('api/', include('favorites.urls')),
+    path('api/', include('favourites.urls')),
 ]
 
 handler404 = TemplateView.as_view(template_name='index.html')
