3a5a5f6a83aff41ed80a31b2b3608403ae0d35df
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-19 13:34:36 +0000

    Adding error handling to loginform

--- src/auth/SignInForm.js
+++ src/auth/SignInForm.js
@@ -1,107 +1,109 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Container, Form, Button, Alert,
-} from 'react-bootstrap';
-import { axiosReq } from '../api/AxiosDefaults';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
 
-function LogInForm() {
-  const navigate = useNavigate();
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { Link, useNavigate } from "react-router-dom";
 
-  const [logInData, setLogInData] = useState({
-    username: '',
-    password: '',
+import { useSetCurrentUser } from "./AuthContext";
+import { useRedirect } from "../api/UseRedirect";
+import { setTokenTimestamp } from "../api/utils";
+
+function SignInForm() {
+  const setCurrentUser = useSetCurrentUser();
+  useRedirect("loggedIn");
+
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
   });
+  const { username, password } = signInData;
 
-  const { username, password } = logInData;
   const [errors, setErrors] = useState({});
 
+  const history = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
+      setTokenTimestamp(data);
+      history.goBack();
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
   const handleChange = (event) => {
-    setLogInData({
-      ...logInData,
+    setSignInData({
+      ...signInData,
       [event.target.name]: event.target.value,
     });
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const { data } = await axiosReq.post('/users/login/', logInData);
-      if (data && data.access_token) {
-        localStorage.setItem('mosaicminds', data.access_token); 
-        navigate('/'); 
-      }
-    } catch (err) {
-      if (err.response) {
-        setErrors(err.response.data); 
-      } else {
-        setErrors({ non_field_errors: ["An unexpected error occurred."] });
-      }
-    }
-  };
+  // Log the signInData and errors
+  console.log("signInData:", signInData);
+  console.log("errors:", errors);
 
   return (
-    <Container className={Container}>
-      <Form onSubmit={handleSubmit}>
-        <br />
-        <br />
-        <Form.Group controlId="username">
-          <Form.Label>username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="username"
-            name="username"
-            value={username}
-            onChange={handleChange}
-            aria-label="username"
-          />
-        </Form.Group>
-        {errors.username?.map((message, idx) => (
-          <Alert variant="danger" key={idx}>
-            {message}
-          </Alert>
-        ))}
+    <Row>
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className="p-4">
+          <h1>sign in</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
-        <Form.Group controlId="password">
-          <Form.Label>password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-            aria-label="password"
-          />
-        </Form.Group>
-        {errors.password?.map((message, idx) => (
-          <Alert variant="danger" key={idx}>
-            {message}
-          </Alert>
-        ))}
-
-        <Button variant="primary" type="submit" className={Button}>
-          login
-        </Button>
-        {errors.non_field_errors?.map((message, idx) => (
-          <Alert variant="danger" key={idx}>
-            {message}
-          </Alert>
-        ))}
-      </Form>
-
-      <br />
-      <div>
-        <p>
-          Don&apos;t have an account?
-          <Link to="/SignUpForm" className={Link}>
-            <strong> Sign up!</strong>
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Button type="submit">Sign in</Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container className="mt-3">
+          <Link to="/signup">
+            Don't have an account? <span>Sign up now!</span>
           </Link>
-        </p>
-      </div>
-      <br />
-    </Container>
+        </Container>
+      </Col>
+    </Row>
   );
 }
 
-export default LogInForm;
+export default SignInForm;
