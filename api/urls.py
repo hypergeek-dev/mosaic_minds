722a8a2bb7fa691fe56db5933da6c6d475f4e879
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-19 21:34:44 +0000

    Setting up runtime protocol for heroku deployment

--- api/urls.py
+++ api/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import path, include
-from .views import logout_route
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
