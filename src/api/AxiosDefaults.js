231f9b72ce6c6034b3a912158516ac812a957c39
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-19 13:30:45 +0000

    Changing paths in axios default to fix faulty api request(500)

--- src/api/AxiosDefaults.js
+++ src/api/AxiosDefaults.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://8000-hypergeekde-mosaicminds-87cb1r5j6l6.ws-eu107.gitpod.io';
+axios.defaults.baseURL = '/api';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
-
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
