f9acfe0a8060729784d8c44ff2140ae2e36b8d78
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-19 13:22:35 +0000

    changing serializer to fit new profiles app

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,8 @@
 from dj_rest_auth.serializers import UserDetailsSerializer
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    class Meta(UserDetailsSerializer.Meta):
-       fields = UserDetailsSerializer.Meta.fields + ('profile_id',)
-
-        
-
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
