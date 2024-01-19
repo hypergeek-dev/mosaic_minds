3d7319d134d8b1bc756933d39799c31dd76ba032
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-19 13:36:42 +0000

    Adding paths for login administration, usercontext and axio requests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useCurrentUser } from "./contexts/CurrentUserContext";
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import "./api/AxiosDefaults.js";
 import './Global.css';
 import NavBar from './components/NavBar';
 import Home from './components/LandingPageHero';
@@ -9,13 +9,20 @@ import MeetingList from './components/MeetingList';
 import Volunteer from './components/Volunteer';
 import MeetingSearchPage from './components/MeetingSearchPage';
 import SignupForm from './auth/SignUpForm';
-import MeetingCreateForm from './components/MeetingCreateForm.js';
-import MeetingDetails from './components/MeetingDetails.js';
-import MeetingEditForm from './components/MeetingEditForm.js';
-import ProfileDetails from './components/ProfileDetails.js';
-import ProfileEditForm from './components/ProfileEditForm.js';
+import { useCurrentUser } from "./auth/AuthContext";
+import MeetingCreateForm from './components/MeetingCreateForm';
+import MeetingDetails from './components/MeetingDetails';
+import MeetingEditForm from './components/MeetingEditForm';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileEditForm from './components/ProfileEditForm';
+import ProfilePage from "./components/ProfilePage";
+import UsernameForm from "./components/UsernameForm";
+import UserPasswordForm from "./components/UserPasswordForm";
+import NotFound from "./components/NotFound.js"
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
   return (
     <Router>
       <div className="App">
@@ -32,10 +39,27 @@ function App() {
           <Route exact path="/meetings/:id/edit" render={() => <MeetingEditForm />} />
           <Route exact path="/profiles/:id" render={() => <ProfileDetails />} />
           <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+          <Route
+            exact
+            path="/profiles/:id/edit/username"
+            render={() => <UsernameForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit/password"
+            render={() => <UserPasswordForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit"
+            render={() => <ProfileEditForm />}
+          />
+
+          <Route render={() => <NotFound />} />
+         </Routes>
+        </div>
+      </Router>
+    )
+};
 
 export default App;
