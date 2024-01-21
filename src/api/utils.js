281afd6e0fd2266177818b987f3ed983b6676c9e
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-21 21:51:44 +0000

    Removing custom refresh code from utils

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,22 +1,5 @@
-import jwtDecode from 'jwt-decode';
-import { axiosReq } from './AxiosDefaults';
-
-
-const refreshAccessToken = async () => {
-  try {
-
-    const response = await axiosReq.post('/dj-rest-auth/token/refresh/');
-    const newAccessToken = response.data.access_token;
-
-    localStorage.setItem('accessToken', newAccessToken);
-
-
-    axiosReq.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
-  } catch (error) {
- 
-    console.error('Token refresh failed:', error);
-  }
-};
+import jwtDecode from "jwt-decode";
+import { axiosReq } from "./AxiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
   try {
@@ -30,33 +13,19 @@ export const fetchMoreData = async (resource, setResource) => {
           : [...acc, cur];
       }, prevResource.results),
     }));
-  } catch (err) {
-  
-    console.error('Fetch data error:', err);
-  }
+  } catch (err) {}
 };
+
 
 export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
-  localStorage.setItem('refreshTokenTimestamp', refreshTokenTimestamp);
+  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
 };
 
 export const shouldRefreshToken = () => {
-  const accessToken = localStorage.getItem('accessToken');
-  if (!accessToken) {
-    return true; 
-  }
-
-  const accessTokenExp = jwtDecode(accessToken).exp;
-  const currentTime = Math.floor(Date.now() / 1000);
-  const refreshTokenTimestamp = parseInt(localStorage.getItem('refreshTokenTimestamp'), 10);
-
-  return currentTime > accessTokenExp || refreshTokenTimestamp <= currentTime;
+  return !!localStorage.getItem("refreshTokenTimestamp");
 };
 
 export const removeTokenTimestamp = () => {
-  localStorage.removeItem('refreshTokenTimestamp');
+  localStorage.removeItem("refreshTokenTimestamp");
 };
-
-
-refreshAccessToken();
