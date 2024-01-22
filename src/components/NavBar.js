619a32725f1c334b80a466154b5289ee7f402af4
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-22 22:07:59 +0000

    Adding logout functionality to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,36 +3,44 @@ import { Link } from "react-router-dom";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import SignInForm from "../auth/SignInForm";
 import jwtDecode from 'jwt-decode';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDoorOpen } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
-const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-const checkAuthentication = () => {
-const token = localStorage.getItem('mosaic.minds');
-if (!token) {
-return false;
-}
-try {
-  const decodedToken = jwtDecode(token);
-  if (decodedToken.exp * 1000 < Date.now()) {
+
+  const checkAuthentication = () => {
+    const token = localStorage.getItem('mosaic.minds');
+    if (!token) {
+      return false;
+    }
+    try {
+      const decodedToken = jwtDecode(token);
+      if (decodedToken.exp * 1000 < Date.now()) {
+        localStorage.removeItem('mosaic.minds');
+        return false;
+      }
+      return true;
+    } catch (error) {
+      console.error('Error decoding token:', error);
+      return false;
+    }
+  };
+
+
+  useEffect(() => {
+    setIsAuthenticated(checkAuthentication());
+  }, []);
+
+
+  const handleLogout = () => {
     localStorage.removeItem('mosaic.minds');
-    return false;
-  }
+    setIsAuthenticated(false);
+  };
 
-  return true;
-} catch (error) {
-  console.error('Error decoding token:', error);
-  return false;
-}
-};
-
-useEffect(() => {
-setIsAuthenticated(checkAuthentication());
-}, []);
-
-return (
-
-<Navbar expand="md">
+  return (
+    <Navbar expand="md">
       <Container>
         <Navbar.Brand as={Link} to="/"></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -46,10 +54,19 @@ return (
               <Nav.Link as={Link} to="/meetings/create">Add Meeting</Nav.Link>
             )}
           </Nav>
-          {<SignInForm />} 
+          <Nav className="ml-auto">
+            {!isAuthenticated ? (
+              <SignInForm />
+            ) : (
+              <Nav.Link onClick={handleLogout}>
+                <FontAwesomeIcon icon={faDoorOpen} /> Logout
+              </Nav.Link>
+            )}
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 };
+
 export default NavBar;
