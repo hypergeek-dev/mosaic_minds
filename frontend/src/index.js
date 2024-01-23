ff4f5c6095d19fb50a59a78d83a39a0bde876070
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-23 22:41:28 +0000

    Refactor app initialization with Router and CurrentUserProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,25 @@
 import React from "react";
-import "./Global.css";
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createRoot } from 'react-dom/client';
-import axios from "axios";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserProvider } from "./auth/AuthContext";
+import "./Global.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+ReactDOM.render(
+  <Router>
+    <CurrentUserProvider>
 
-axios.defaults.baseURL = "/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
+        <App />
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+    </CurrentUserProvider>
+  </Router>,
+  document.getElementById("root")
+);
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
