a756790c9bfb86a612d658ac480a17069712212e
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-24 12:52:24 +0000

    Removing package.json from root

--- frontend/src/auth/SignInForm.js
+++ frontend/src/auth/SignInForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useSetCurrentUser } from "../auth/AuthContext";
+import { useSetCurrentUser } from "./AuthContext";
 import { useRedirect } from "../api/UseRedirect";
 import { setTokenTimestamp } from "../api/utils";
 import Form from "react-bootstrap/Form";
