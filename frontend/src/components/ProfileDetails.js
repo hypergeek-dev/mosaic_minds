0bff658ff93ff0cbfc988807a3961a01493ac9a6
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-24 20:40:36 +0000

    Enhancing ProfileDetails Component with User-Specific Data Display

--- frontend/src/components/ProfileDetails.js
+++ frontend/src/components/ProfileDetails.js
@@ -2,46 +2,44 @@ import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import { useParams } from "react-router";
 import { axiosReq } from "../api/AxiosDefaults";
+import { useCurrentUser } from "../auth/AuthContext";
 
 function ProfileDetails() {
-  // State for loading status and profile details
   const [hasLoaded, setHasLoaded] = useState(false);
   const [profile, setProfile] = useState({});
+  const currentUser = useCurrentUser();
 
-  // Extract profile ID from URL
-  const { id } = useParams();
-
-  // Effect to fetch profile details when component mounts or profile ID changes
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axiosReq.get(`/profiles/${id}/`);
-        setProfile(data);
-        setHasLoaded(true);
-      } catch (err) {
-        // Error handling (currently commented out)
-        // console.log(err);
-      }
-    };
-    fetchData();
-  }, [id]);
+    if (currentUser) {
+      const fetchData = async () => {
+        try {
+          const { data } = await axiosReq.get(`/profiles/${currentUser.id}/`);
+          setProfile(data);
+          setHasLoaded(true);
+        } catch (err) {
+          // Error handling
+        }
+      };
+      fetchData();
+    }
+  }, [currentUser]);
 
-  // JSX for displaying the profile details
+  // Adapted JSX for displaying the profile details
   const profileDetails = (
-    <Row noGutters className="px-3 text-center">
-      <Col>
-        <h3 className="m-2">{profile.owner}</h3>
-        <div>Email: {profile.email}</div>
-        <div>Phone Number: {profile.phonenumber}</div>
-        <div>Role: {profile.role_at_meeting}</div>
-        <div>Joined: {profile.created_at}</div>
-      </Col>
-    </Row>
+    <Row nogutters="true" className="px-3 text-center">
+    <Col>
+      <h3 className="m-2">{profile?.owner || 'No Owner'}</h3>
+      <div><p>First Name: {profile?.first_name || 'No First Name'}</p></div>
+      <div><p>Last Name: {profile?.last_name || 'No Last Name'}</p></div>
+      <div><p>Email: {profile?.email || 'No Email'}</p></div>
+      <div><p>Phone Number: {profile?.phonenumber || 'No Phone Number'}</p></div>
+      <div><p>Role: {profile?.role_at_meeting || 'No Role'}</p></div>
+      <div><p>Joined: {profile?.created_at || 'No Join Date'}</p></div>
+    </Col>
+  </Row>
   );
 
-  // Main return statement rendering the profile details
   return (
     <Container>
       {hasLoaded ? profileDetails : <p>Loading...</p>}
