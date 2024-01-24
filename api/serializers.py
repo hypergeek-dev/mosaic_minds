1d90fae8b93159a686a02721630c11867e4021fd
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-24 16:33:14 +0000

    Adding check to see if user is superuser in serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
+    is_superuser = serializers.BooleanField(read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_superuser')
