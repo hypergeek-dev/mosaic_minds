f821db079d9bf2761c103fd210c1c7d6cf685128
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-25 01:31:24 +0000

    Adding edit links to urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-    path('profiles/', views.ProfileList.as_view()),
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+    path('profiles/', views.ProfileList.as_view(), name='profile-list'),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view(), name='profile-detail'),
+    path('profiles/<int:pk>/edit/', views.ProfileDetail.as_view(), name='meeting-edit'),
 ]
