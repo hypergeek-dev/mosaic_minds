78fb9704b7f7da949ca5c85fb7b4b9055b4bc208
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-05-06 20:37:25 +0000

    Adding toast for alert capability

--- frontend/src/pages/VolunteerForm.js
+++ frontend/src/pages/VolunteerForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Form, Button, Container, Card, Row, Col } from 'react-bootstrap';
-import axios from 'axios'; 
+import { Form, Button, Container, Card, Row, Col, Alert } from 'react-bootstrap'; 
+import axios from 'axios';
 
 const VolunteerForm = () => {
     const [formData, setFormData] = useState({
@@ -8,6 +8,8 @@ const VolunteerForm = () => {
         email: '',
         motivation: ''
     });
+
+    const [submitResponse, setSubmitResponse] = useState(''); 
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -18,7 +20,7 @@ const VolunteerForm = () => {
         try {
             const response = await axios.post('/volunteer/', formData);
             console.log(response.data);
-            alert('Application submitted successfully!');
+            setSubmitResponse('Application submitted successfully!');
             setFormData({
                 name: '',
                 email: '',
@@ -26,20 +28,19 @@ const VolunteerForm = () => {
             });
         } catch (error) {
             console.error('There was an error submitting the form:', error);
-            alert('Failed to submit application.');
+            setSubmitResponse('Failed to submit application. Please try again.');
         }
-    };
+    };    
 
     return (
-<Container className="my-4 form-container-custom">
-    <Row className="justify-content-center">
-    <Col md={8} lg={12}> 
-
+        <Container className="my-4 form-container-custom">
+            <Row className="justify-content-center">
+                <Col md={8} lg={12}>
                     <Card>
                         <Card.Header as="h3" className="text-center">Volunteer Application</Card.Header>
                         <Card.Body>
                             <Form onSubmit={handleSubmit} aria-label="Volunteer Application Form">
-                        
+
                                 <Form.Group className="mb-3">
                                     <Form.Label htmlFor="name">Name</Form.Label>
                                     <Form.Control
@@ -84,6 +85,8 @@ const VolunteerForm = () => {
                                     <Button variant="primary" type="submit" aria-label="Submit Button">Submit</Button>
                                 </div>
                             </Form>
+                            {/* Display toast notification */}
+                            {submitResponse && <Alert variant={submitResponse.includes('successfully') ? 'success' : 'danger'}>{submitResponse}</Alert>}
                         </Card.Body>
                     </Card>
                 </Col>
