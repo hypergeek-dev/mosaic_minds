fb5062328ec469a49c876421e5e0385147f49a74
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-05-06 20:37:53 +0000

    Styling profilepage

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ProfileDetails from '../components/ProfileDetails';
+import ProfileEditForm from '../components/ProfileEditForm';
 import FavoritesList from '../components/FavoritesList';
 
 const ProfilePage = () => {
+    const [editMode, setEditMode] = useState(false);
+
     return (
         <Container fluid> 
-         
             <Row className="mt-4"> 
                 <Col xs={12}> 
-                    <ProfileDetails />
+                    {editMode ? (
+                        <ProfileEditForm onCancel={() => setEditMode(false)} />
+                    ) : (
+                  
+                        <ProfileDetails />
+                    )}
+
                 </Col>
             </Row>
             
-      
             <Row className="mt-4"> 
                 <Col xs={12}> 
                     <h2 className="text-center">Favorites</h2> 
+                    <FavoritesList />
                 </Col>
             </Row>
         </Container>
