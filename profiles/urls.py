304c1487a6fe6baaf2ae107eb9d112ca25d9fb44
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-05-06 20:38:16 +0000

    Refactoring urls for proper naming convention

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from profiles import views
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view(), name='profile-list'),
     path('profiles/<int:pk>/', views.ProfileDetail.as_view(), name='profile-detail'),
-    path('profiles/<int:pk>/edit/', views.ProfileDetail.as_view(), name='meeting-edit'),
+    path('profiles/<int:pk>/edit/', views.ProfileDetail.as_view(), name='profile-edit'),
+
 ]
