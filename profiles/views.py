14b8be913db87486f07cc2a31f0dc32068a86c05
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-05-06 20:40:16 +0000

    Updated ProfileDetail view to improve code readability.

--- profiles/views.py
+++ profiles/views.py
@@ -10,14 +10,14 @@ class ProfileList(generics.ListCreateAPIView):
     """
     queryset = Profile.objects.all().order_by('-created_at')
     serializer_class = ProfileSerializer
-
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve or update a profile if you're the owner.
     """
+    queryset = Profile.objects.all() 
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
 
     def get_queryset(self):
         user = self.request.user
-        return Profile.objects.filter(owner=user)
+        return self.queryset.filter(owner=user)
